ef8f5c2e3e783632bd0a9b888153ef55bc35d5d8
Yuresh Tharushika <yuresh.official@gmail.com>
2024-06-08 17:48:48 +0530

    project initialize

---
+++ src/app/expense-form/expense-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-expense-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './expense-form.component.html',
+  styleUrl: './expense-form.component.css'
+})
+export class ExpenseFormComponent {
+
+}
